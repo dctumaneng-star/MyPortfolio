5acc10a26477dc8e6369af02f65383e552e57d7c
daryltumaneng <239285206+spoterdarylhan-star@users.noreply.github.com>
2026-09-23 23:01:57 +0800

    style: revert typeface to Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,8 +22,8 @@ export default {
         },
       },
       fontFamily: {
-        display: ["'Space Grotesk'", "sans-serif"],
-        body:    ["'Space Grotesk'", "sans-serif"],
+        display: ["'Inter'", "sans-serif"],
+        body:    ["'Inter'", "sans-serif"],
         mono:    ["'JetBrains Mono'", "monospace"],
       },
       fontSize: {
